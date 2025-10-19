efa27e10f9e1fbd06cc875669755def83ed58764
LazarevskiGoce16 <lazarevskigoce26@gmail.com>
2025-10-19 05:06:56 +0200

    changed localhost to mssqllocaldb for db connection

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -5,16 +5,16 @@ dotenv.config();
 
 const sequelize = new Sequelize({
     database: process.env.DB_NAME || "expense_tracker",
-    username: process.env.DB_USER || "sa",
+    username: process.env.DB_USER || "",
     password: process.env.DB_PASSWORD || "",
-    host: process.env.DB_HOST || "localhost",
-    port: parseInt(process.env.DB_PORT || "1433"),
+    host: process.env.DB_HOST || "(localdb)\\MSSQLLocalDB",
     dialect: "mssql",
     dialectOptions: {
         options: {
             encrypt: process.env.DB_ENCRYPT === 'true',
             trustServerCertificate: process.env.DB_TRUST_CERT === 'true',
             enableArithAbort: true,
+            integratedSecurity: true
         },
     },
     logging: process.env.NODE_ENV === 'development' ? console.log : false,
